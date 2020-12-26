ebee31cd6391051f9fb6d94c503e3264461065d9
= <=>
2020-12-26 22:56:41 +0800

    计算给定二叉树的所有左叶子之和

---
+++ src/main/java/leedCode/LeedCode_404.java
@@ -0,0 +1,47 @@
+package leedCode;
+
+import common.TreeNode;
+import common.TreeUtil;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 计算给定二叉树的所有左叶子之和。
+ * */
+public class LeedCode_404 {
+    public int sumOfLeftLeaves(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int ans = 0;
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode preNode = null;
+        while(!stack.isEmpty() || root != null){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            preNode = stack.peek();
+
+            if(root.left == null && root.right == null && preNode.left == root){
+                ans += root.val;
+            }
+
+            root = root.right;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String trees = "[4,-7,-3,null,null,9,-7,-4,null,-2]";
+        TreeNode root = TreeUtil.covertToTreeNode(trees);
+
+        LeedCode_404 solution = new LeedCode_404();
+        int ans = solution.sumOfLeftLeaves(root);
+        System.out.println(ans);
+    }
+}
