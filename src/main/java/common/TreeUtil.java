1e87518d8509c2c389ec699b613228aaa6e03be1
= <=>
2020-12-26 22:36:44 +0800

    序列化与反序列化二叉树

--- src/main/java/common/TreeUtil.java
+++ src/main/java/common/TreeUtil.java
@@ -1,57 +1,52 @@
 package common;
 
-import org.apache.commons.lang.StringUtils;
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
- *
+ * 序列化与反序列化二叉树
  * */
 public class TreeUtil {
-    private static final String PREFIX_JSON = "[";
-    private final static String SUBFIX_JSON = "]";
-
     public static TreeNode covertToTreeNode(String json) throws Exception {
-        if(StringUtils.isBlank(json)){
+        if(isBlank(json)){
             return null;
         }
 
-        if(!json.startsWith(PREFIX_JSON) && !json.endsWith(SUBFIX_JSON)){
+        if(!json.startsWith("[") && !json.endsWith("]")){
             throw new Exception("格式不正确");
         }
 
         String[] data = json.substring(1, json.length() - 1).split(",");
 
-        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
         TreeNode root = new TreeNode(Integer.valueOf(data[0]));
-        stack.offer(root);
+        queue.offer(root);
 
         int len = data.length, index = 0, left = 0, right = 0;
-        while(!stack.isEmpty() && index < len){
-            TreeNode curNode = stack.pop();
+        while(!queue.isEmpty()){
+            TreeNode curNode = queue.pop();
 
             left = (2 * index) + 1;
 
             if(left >= len){
-                continue;
+                //System.out.println("left = " + left + " queue = " + queue);
+                break;
             }
 
             TreeNode leftNode = createNodeByNum(data[left]);
             curNode.left = leftNode;
             if(null != leftNode){
-                stack.offer(leftNode);
+                queue.offer(leftNode);
             }
 
             right = (index + 1) * 2;
             if(right >= len){
-                continue;
+                //System.out.println("right = " + right + " queue = " + queue);
+                break;
             }
             TreeNode rightNode = createNodeByNum(data[right]);
 
             if(null != rightNode){
-                stack.offer(rightNode);
+                queue.offer(rightNode);
             }
             curNode.right = rightNode;
 
@@ -60,35 +55,74 @@ public class TreeUtil {
         return root;
     }
 
+    public static String covertTreeNodeToString(TreeNode root){
+        if(root == null){
+            return "[]";
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+
+        String ans = "";
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            if(nullQueue(queue)){
+                queue.removeAll(queue);
+                continue;
+            }
+
+            while(size > 0){
+                TreeNode curNode = queue.pollFirst();
+                if(curNode == null && !queue.isEmpty()){
+                    ans += "null,";
+                    size--;
+                    continue;
+                } if(curNode != null) {
+                    ans += curNode.val + ",";
+
+                    queue.offer(curNode.left);
+                    queue.offer(curNode.right);
+                }
+
+                size--;
+            }
+        }
+
+        if(ans.length() > 0){
+            ans = ans.substring(0, ans.length() - 1);
+        }
+        return "[" + ans + "]";
+    }
+
+    private static boolean nullQueue(LinkedList<TreeNode> queue){
+        for(TreeNode node : queue){
+            if(node != null){
+               return false;
+            }
+        }
+        return true;
+    }
+
     private static TreeNode createNodeByNum(String num){
-        if(StringUtils.isBlank(num) || num.equals("null")){
+        if(isBlank(num) || num.equals("null")){
             return null;
         }
 
         return new TreeNode(Integer.valueOf(num));
     }
 
-    public static void main(String[] args) throws Exception {
-        String treeJson = "[4,2,7,1,null,6,9]";
-        TreeNode root = covertToTreeNode(treeJson);
-
-        inOrder(root);
+    private static boolean isBlank(String str){
+        if(str == null || str.length() == 0){
+            return true;
+        }
+        return false;
     }
 
-    public static void inOrder(TreeNode root){
-        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
-        while(!stack.isEmpty() || null != root){
-            while(null != root){
-                stack.push(root);
-                root = root.left;
-            }
-
-            root = stack.pop();
-
-            System.out.print(root.val + " ");
-
-            root = root.right;
-        }
-        System.out.println();
+    public static void main(String[] args) throws Exception {
+        //String treeJson = "[4,2,7,1,null,6,9]";
+        String treeJson = "[4,-7,-3,null,null,-4,null,6,null,-2]";
+        TreeNode root = covertToTreeNode(treeJson);
+        String ans = covertTreeNodeToString(root);
+        System.out.println(ans);
     }
 }
