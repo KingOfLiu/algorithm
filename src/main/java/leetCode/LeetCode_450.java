1aa7e81dbb6b9869e018a08c71709546f43e4280
= <=>
2020-12-31 12:27:05 +0800

    删除二叉搜索树中的节点

---
+++ src/main/java/leetCode/LeetCode_450.java
@@ -0,0 +1,69 @@
+package leetCode;
+
+import common.TreeNode;
+import common.TreeUtil;
+
+/**
+ *  删除二叉搜索树中的节点
+ *  给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。
+ *  返回二叉搜索树（有可能被更新）的根节点的引用。
+ *
+ * 一般来说，删除节点可分为两个步骤：
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ *
+ * 说明： 要求算法时间复杂度为 O(h)，h 为树的高度。
+ * */
+public class LeetCode_450 {
+    public TreeNode deleteNode(TreeNode root, int key){
+        if(root == null){
+            return null;
+        }
+
+        TreeNode preNode = null;
+        TreeNode curNode = root;
+        while(curNode != null){
+            if(curNode.val == key){
+                if(curNode.left == null && curNode.right == null){
+                    curNode = null;
+                }else if(curNode.left != null){
+                    TreeNode temp = curNode;
+                    if(preNode != null){
+                        preNode = null;
+                    }
+                } else {
+                    TreeNode temp = curNode;
+                    TreeNode tempLeft = curNode.left;
+                    TreeNode tempRight = curNode.right;
+                    curNode = tempRight;
+                    curNode.left = tempLeft;
+
+                    if(preNode != null && preNode.left == temp){
+                        preNode.left = curNode;
+                    } else if(preNode != null && preNode.right == temp){
+                        preNode.right = curNode;
+                    }
+                }
+                break;
+            } else if(curNode.val > key){
+                preNode = curNode;
+                curNode = curNode.left;
+            } else {
+                preNode = curNode;
+                curNode = curNode.right;
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int k = 3;
+        //String trees = "[5,3,6,2,4,null,7]";
+        String trees = "[2,3]";
+        TreeNode root = TreeUtil.covertToTreeNode(trees);
+
+        LeetCode_450 solution = new LeetCode_450();
+        TreeNode ans = solution.deleteNode(root, k);
+        System.out.println(TreeUtil.covertTreeNodeToString(ans));
+    }
+}
