d747b15019d34bea68ff44e0c3be595882633b31
KingOfLiu <15598114232@163.com>
2021-01-10 18:07:21 +0800

    接雨水

--- src/main/java/leetCode/array/LeetCode_42.java
+++ src/main/java/leetCode/array/LeetCode_42.java
@@ -1,5 +1,8 @@
 package leetCode.array;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 接雨水
  * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
@@ -8,6 +11,9 @@ package leetCode.array;
  * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
  * */
 public class LeetCode_42 {
+    /**
+     * 暴力实现
+     * */
     public int trapViolence(int[] height){
         int len = height.length;
         if(len == 0){
@@ -33,6 +39,9 @@ public class LeetCode_42 {
         return ans;
     }
 
+    /**
+     * 动态规划实现，记录左右两边最大高度
+     * */
     public int trapDp(int[] height){
         int len = height.length;
         if(len == 0){
@@ -58,12 +67,67 @@ public class LeetCode_42 {
         return ans;
     }
 
+    /**
+     * 单调栈实现
+     * */
+    public int trapStack(int[] height){
+        int len = height.length;
+        if(len == 0){
+            return 0;
+        }
+
+        int ans = 0;
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        for(int i = 0; i < len; i++){
+            while(!stack.isEmpty() && height[stack.peek()] < height[i]){
+                int top = stack.pop();
+                if(stack.isEmpty()){
+                    break;
+                }
+                int distance = i - stack.peek() - 1;
+                int boundedHeight = Math.min(height[stack.peek()], height[i]) - height[top];
+                ans += distance * boundedHeight;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+    public int trapDoublePointer(int[] height){
+        int len = height.length;
+        if(len == 0){
+            return 0;
+        }
+
+        int ans = 0;
+        int left = 0, right = len - 1;
+        int maxLeft = 0, maxRight = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= maxLeft){
+                    maxLeft = Math.max(maxLeft, height[left]);
+                } else {
+                    ans += (maxLeft - height[left]);
+                }
+                left ++;
+            } else {
+                if(height[right] >= maxRight){
+                    maxRight = Math.max(maxRight, height[right]);
+                } else {
+                    ans += (maxRight - height[right]);
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
-        //int[] height = {4,2,0,3,2,5};
+        int[] height = {4,2,0,3,2,5};
         //int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int[] height = {6, 7, 5,2,4,5,9,3};
+        //int[] height = {6, 7, 5,2,4,5,9,3};
         LeetCode_42 solution = new LeetCode_42();
-        int ans = solution.trapDp(height);
+        int ans = solution.trapDoublePointer(height);
         System.out.println(ans);
     }
 }
