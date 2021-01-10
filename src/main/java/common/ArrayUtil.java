8e94bc37d0fa744c9290325bf6dfaa2c31400229
KingOfLiu <15598114232@163.com>
2021-01-10 20:54:00 +0800

    添加打印数组重载方法

--- src/main/java/common/ArrayUtil.java
+++ src/main/java/common/ArrayUtil.java
@@ -4,12 +4,42 @@ package common;
  * 数组工具类
  * */
 public class ArrayUtil {
+    public static void printArray(int[] nums, boolean lineFeed){
+        if(nums == null){
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+
+        if(lineFeed){
+            System.out.println();
+        }
+    }
+
+    public static void printArray(int[] nums){
+        printArray(nums, true);
+    }
+
     public static void printArray(int[][] nums){
         if(nums == null){
             return;
         }
         for(int i = 0; i < nums.length; i++){
             int[] row = nums[i];
+            for(int j = 0; j < row.length; j++){
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printArray(char[][] nums){
+        if(nums == null){
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            char[] row = nums[i];
             for(int j = 0; j < row.length; j++){
                 System.out.print(nums[i][j] + " ");
             }
