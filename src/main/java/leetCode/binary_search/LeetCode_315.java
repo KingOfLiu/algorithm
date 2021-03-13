25d60968ba2b83ded14ca395e190c768d97fc3d7
= <=>
2021-03-13 08:31:26 +0800

    315. 计算右侧小于当前元素的个数

--- src/main/java/leetCode/binary_search/LeetCode_315.java
+++ src/main/java/leetCode/binary_search/LeetCode_315.java
@@ -61,6 +61,41 @@ public class LeetCode_315 {
         return start;
     }
 
+    /** 二分查找树——超时！ */
+    /*class Node{
+        Node left, right;
+        int val, sum, dup = 1;
+        Node(int v, int s){
+            val = v;
+            sum = s;
+        }
+    }
+
+    private Node insert(int num, Node node, Integer[] ans, int i, int preSum){
+        if(node == null){
+            node = new Node(num, 0);
+            ans[i] = preSum;
+        } else if(node.val == num){
+            node.dup++;
+            ans[i] = preSum + node.sum;
+        } else if(node.val > num){
+            node.sum++;
+            node.left = insert(num, node.left, ans, i, preSum);
+        } else {
+            node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
+        }
+        return node;
+    }
+
+    public List<Integer> countSmaller(int[] nums){
+        Integer[] ans = new Integer[nums.length];
+        Node root = null;
+        for(int i = nums.length - 1; i >= 0; i--){
+            root = insert(nums[i], root, ans, i, 0);
+        }
+        return Arrays.asList(ans);
+    }*/
+
     public static void main(String[] args){
         int[] nums = {5,2,6,1,7,3};
 
