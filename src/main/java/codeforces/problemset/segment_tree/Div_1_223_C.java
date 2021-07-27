fef8981c3819dcde1f339fc281115bd7f391e6d0
= <=>
2021-07-28 07:46:31 +0800

    C. Sereja and Brackets——线段树

--- src/main/java/codeforces/problemset/segment_tree/Div_1_223_C.java
+++ src/main/java/codeforces/problemset/segment_tree/Div_1_223_C.java
@@ -9,6 +9,11 @@ import java.util.Scanner;
 public class Div_1_223_C {
     static class Node {
         int a, b, num;
+        int left, right;
+
+        public String toString(){
+            return "left = " + left + " right = " + right + " a = " + a + " b = " + b + " num = " + num;
+        }
     }
 
     static class SegmentTree {
@@ -25,12 +30,16 @@ public class Div_1_223_C {
             if(left == right){
                 char ch = data[left];
                 nodes[treeIndex] = new Node();
+                nodes[treeIndex].left = left;
+                nodes[treeIndex].right = right;
                 nodes[treeIndex].a = (ch == '(' ? 1 : 0);
                 nodes[treeIndex].b = (ch == ')' ? 1 : 0);
                 return ;
             }
 
             nodes[treeIndex] = new Node();
+            nodes[treeIndex].left = left;
+            nodes[treeIndex].right = right;
 
             int leftIndex = leftChild(treeIndex);
             int rightIndex = rightChild(treeIndex);
@@ -39,12 +48,47 @@ public class Div_1_223_C {
             buildSegmentTree(leftIndex, left, mid);
             buildSegmentTree(rightIndex, mid + 1, right);
 
-            Node leftNode = nodes[leftIndex], rightNode = nodes[rightIndex];
-            if(leftNode != null && rightNode != null){
-                if(leftNode.a != 0 && rightNode.b != 0){
-                    nodes[treeIndex].num += 2 * Math.min(leftNode.a, rightNode.b);
-                }
+            int x = Math.min(nodes[leftIndex].a, nodes[rightIndex].b);
+            nodes[treeIndex].num = x + nodes[leftIndex].num + nodes[rightIndex].num;
+            nodes[treeIndex].a = nodes[leftIndex].a + nodes[rightIndex].a - x;
+            nodes[treeIndex].b = nodes[leftIndex].b + nodes[rightIndex].b - x;
+        }
+
+        Node query(int treeIndex, int l, int r, int left, int right){
+            // 区间完全包含的条件
+            if(l == left && r == right){
+                return nodes[treeIndex];
             }
+
+            int mid = l + (r - l) / 2;
+            int leftIndex = leftChild(treeIndex), rightIndex = rightChild(treeIndex);
+            if(left > mid){
+                 return query(rightIndex, mid + 1, r, left, right);
+            }
+
+            if(right <= mid){
+                return query(leftIndex, l, mid, left, right);
+            }
+
+            Node leftNode = query(leftIndex, l, mid, left, mid);
+            Node rightNode = query(rightIndex, mid + 1, r, mid + 1, right);
+
+            Node result = new Node();
+            int x = Math.min(leftNode.a, rightNode.b);
+            result.num += x;
+
+            result.a += rightNode.a;
+            result.b += rightNode.b;
+            result.num += rightNode.num;
+
+            result.a += leftNode.a;
+            result.b += leftNode.b;
+            result.num += leftNode.num;
+
+            result.a -= x;
+            result.b -= x;
+
+            return result;
         }
 
         private int leftChild(int index){
@@ -60,12 +104,18 @@ public class Div_1_223_C {
         Scanner sc = new Scanner(new BufferedInputStream(System.in));
         String words = sc.next();
         int m = sc.nextInt();
-        int[] q = new int[m];
+        int[][] q = new int[m][2];
         for(int i = 0; i < m; i++){
-            q[i] = sc.nextInt();
+            q[i][0] = sc.nextInt();
+            q[i][1] = sc.nextInt();
         }
 
         SegmentTree st = new SegmentTree(words);
-        System.out.println(st);
+        for(int i = 0; i < m; i++){
+            int left = q[i][0], right = q[i][1];
+
+            int ans = st.query(0, 0, words.length() - 1, left - 1, right - 1).num * 2;
+            System.out.println(ans);
+        }
     }
 }
